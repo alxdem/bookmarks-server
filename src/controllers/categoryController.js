dd97f3d0c72570cddc358978343b5e0e37889ec3
Alexey Demidov <dr.a.lekter@gmail.com>
2024-12-07 21:32:44 +0400

    Fix CORS

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -11,8 +11,10 @@ class CategoryController {
                 userId: userId,
             });
 
-            // return res.json(data);
-            return data;
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            return res.json(data);
         } catch (err) {
             return res.status(500).json(getError(text.SERVER_ERROR, err));
         }
