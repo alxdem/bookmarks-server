7b1ab26197c7535613bb276d4a08923ec9d6bef1
Alexey Demidov <dr.a.lekter@gmail.com>
2024-12-07 20:36:55 +0400

    Fix CORS

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(categoriesRouter);
 app.use(itemsRouter);
 app.use('/auth', authRouter);
